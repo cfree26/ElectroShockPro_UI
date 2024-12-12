f0a5d3e26c81defd191d0d174d546d3f8bf4dd30
siPyt <sinkinpython@gmail.com>
2024-12-12 16:21:38 -0600

    Add environment.prod.ts

--- capstoneFreeman/src/environments/environment.prod.ts
+++ capstoneFreeman/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://electro-shock-pro-0e357a1a89e6.herokuapp.com/' // Update this with your production API URL
+  apiUrl: 'https://electro-shock-pro-0e357a1a89e6.herokuapp.com/' // Update this with your production API 
 };
 
 // Modification to force Git track
