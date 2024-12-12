cd82705aaf356816a7a6bf35b5c9503161854a7c
siPyt <sinkinpython@gmail.com>
2024-12-12 15:58:11 -0600

    fixed angular.json

--- capstoneFreeman/src/environments/environment.prod.ts
+++ capstoneFreeman/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://electro-shock-pro-0e357a1a89e6.herokuapp.com/' // Update this with your production API URL
 };
+
